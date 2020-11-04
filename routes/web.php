cdb519e67bf4cbb95c191ddf0b1dafd1320702d7
hoangthaonam <hoangthaonampk2407@gmail.com>
2020-11-04 10:14:04 +0700

    create route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/task',function(Request $request){
+
+});
+Route::delete('/task/{id}',function(Task $task){
+
 });
